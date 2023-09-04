b3574f3f29ea35dd082cc774f3a65a521ebc06ef
MirellyFontenele <mirellyfontenele2@gmail.com>
2023-09-03 21:38:29 -0300

    teste Mirelly

---
+++ TrianguloTest.java
@@ -0,0 +1,63 @@
+import org.example.Main;
+import org.example.TrianguloChecker;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.theories.suppliers.TestedOn;
+
+
+public class TrianguloTest {
+    private TrianguloChecker trianguloChecker;
+
+    @After
+    @Before
+    public void before(){
+        this.trianguloChecker = new TrianguloChecker();
+    }
+
+
+    @Test
+    public void trinaguloTestCorrectValues1(){
+        this.trianguloChecker.setLados(2.0, 2.0, 2.0);
+        this.trianguloChecker.verificaTriangulo();
+        final boolean isTriangle = this.trianguloChecker.isTriangle();
+        final String response = this.trianguloChecker.getMessage();
+
+        Assert.assertTrue(isTriangle);
+        Assert.assertEquals("os lados formam um triângulo", response );
+    }
+
+    @Test
+    public void trinaguloTestCorrectValues2(){
+        this.trianguloChecker.setLados(2.0, 3.0, 2.0);
+        this.trianguloChecker.verificaTriangulo();
+        final boolean isTriangle = this.trianguloChecker.isTriangle();
+        final String response = this.trianguloChecker.getMessage();
+
+        Assert.assertTrue(isTriangle);
+        Assert.assertEquals("os lados formam um triângulo", response );
+    }
+
+    @Test
+    public void trinaguloTestCorrectValues3(){
+        this.trianguloChecker.setLados(3.0, 2.0, 3.0);
+        this.trianguloChecker.verificaTriangulo();
+        final boolean isTriangle = this.trianguloChecker.isTriangle();
+        final String response = this.trianguloChecker.getMessage();
+
+        Assert.assertTrue(isTriangle);
+        Assert.assertEquals("os lados formam um triângulo", response );
+    }
+    @Test
+    public void trianguloTestValoresNegativos() {
+        this.trianguloChecker.setLados(-1.0, 2.0, 3.0);
+        this.trianguloChecker.verificaTriangulo();
+        final String response = this.trianguloChecker.getMessage();
+        final boolean isTriangle = this.trianguloChecker.isTriangle();
+
+        Assert.assertFalse(isTriangle);
+        Assert.assertEquals("os lados não podem ser negativos ou igual a zero", response);
+    }
+
+}
